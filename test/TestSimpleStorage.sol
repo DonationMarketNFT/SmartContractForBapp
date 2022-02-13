f616f2237abc721eaefb75678b1a7ea5c5f93e39
downy <TeamBehindDowny@gmail.com>
2022-02-13 10:02:11 +0900

    add test code

---
+++ test/TestSimpleStorage.sol
@@ -0,0 +1,17 @@
+//Klaytn IDE uses solidity 0.4.24 0.5.6 versions.
+pragma solidity >=0.4.24 <=0.5.6;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/NFTSimple.sol";
+
+contract TestSimpleStorage{
+    function testSimpleStorage() public {
+
+        NFTSimple it = new NFTSimple();
+
+        it.mintWithTokenURI(address(this), 20, "happy");
+        uint256 ans = it.balanceOf(address(this));
+        Assert.equal(ans, 3, "value equal test");
+    }
+}
